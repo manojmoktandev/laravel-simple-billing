f5121ba3064223b012d319a1b076e6d43ac53bad
Manoj Moktan <developermanojmoktan@gmail.com>
2024-01-14 18:49:04 -0500

    [added] initial files for billing application

---
+++ README.md
@@ -0,0 +1,16 @@
+# Project Name: Simple Laravel billing application
+
+# Database migration
+    php artisan make:migration create_parties_table --create=parties
+    php artisan make:migration create_gst_bills_table --create=gst_bills
+    php artisan make:migration create_vendor_invoices_table --create=vendor_invoices
+
+
+php artisan migrate
+
+
+## Backend Panel
+    # Parties routes functionality
+    # GstBill routes functionality
+    # Vendor invoice routes functionality
+
---
+++ app/Models/User.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Model
+{
+    use HasFactory;
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+}
